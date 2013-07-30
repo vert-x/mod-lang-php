810c46f258e7acba01ccda552202ee416a788efd
Jordan Halterman <jordan.halterman@gmail.com>
2013-07-29 23:31:42 -0700

    Complete core Container class interface.

--- src/main/java/com/blankstyle/vertx/php/Container.java
+++ src/main/java/com/blankstyle/vertx/php/Container.java
@@ -31,36 +31,127 @@ public final class Container {
     }
   }
 
-  public static void deployModule(final Env env, final StringValue moduleName, @Optional final ArrayValue config, @Optional final Value instances, @Optional final Callback callback) {
-    if (instances.isDefault() && config.isDefault() && callback.isDefault()) {
+  /**
+   * Deploys a module.
+   */
+  public static void deployModule(final Env env, final StringValue moduleName, @Optional final ArrayValue config, @Optional final Value instances, @Optional final Callback handler) {
+    if (instances.isDefault() && config.isDefault() && handler.isDefault()) {
       Container.instance.deployModule(moduleName.toJavaString());
     }
-    else if (!instances.isDefault() && config.isDefault() && callback.isDefault()) {
+    else if (!instances.isDefault() && config.isDefault() && handler.isDefault()) {
       Container.instance.deployModule(moduleName.toJavaString(), instances.toJavaInteger());
     }
-    else if (!instances.isDefault() && config.isDefault() && !callback.isDefault()) {
+    else if (!instances.isDefault() && config.isDefault() && !handler.isDefault()) {
       Container.instance.deployModule(moduleName.toJavaString(), instances.toJavaInteger(), new AsyncResultHandler<String>() {
         @Override
         public void handle(AsyncResult<String> result) {
-          callback.toCallable(env, false).call(env, env.wrapJava(result));
+          handler.toCallable(env, false).call(env, env.wrapJava(result));
         }
       });
     }
-    else if (!instances.isDefault() && !config.isDefault() && !callback.isDefault()) {
+    else if (!instances.isDefault() && !config.isDefault() && !handler.isDefault()) {
       Container.instance.deployModule(moduleName.toJavaString(), new JsonObject(config.toJavaMap(env, new HashMap<String, Object>().getClass())), instances.toJavaInteger());
     }
   }
 
+  /**
+   * Undeploys a module.
+   */
+  public static void undeployModule(final Env env, StringValue deploymentID, @Optional final Callback handler) {
+    if (handler != null && !handler.isDefault()) {
+      Container.instance.undeployModule(deploymentID.toString(), new AsyncResultHandler<Void>() {
+        @Override
+        public void handle(AsyncResult<Void> result) {
+          handler.call(env, env.wrapJava(result));
+        }
+      });
+    }
+    else {
+      Container.instance.undeployModule(deploymentID.toString());
+    }
+  }
+
+  /**
+   * Deploys a verticle.
+   */
+  public static void deployVerticle(final Env env, final StringValue moduleName, @Optional final ArrayValue config, @Optional final Value instances, @Optional final Callback handler) {
+    if (instances.isDefault() && config.isDefault() && handler.isDefault()) {
+      Container.instance.deployVerticle(moduleName.toJavaString());
+    }
+    else if (!instances.isDefault() && config.isDefault() && handler.isDefault()) {
+      Container.instance.deployVerticle(moduleName.toJavaString(), instances.toJavaInteger());
+    }
+    else if (!instances.isDefault() && config.isDefault() && !handler.isDefault()) {
+      Container.instance.deployVerticle(moduleName.toJavaString(), instances.toJavaInteger(), new AsyncResultHandler<String>() {
+        @Override
+        public void handle(AsyncResult<String> result) {
+          handler.toCallable(env, false).call(env, env.wrapJava(result));
+        }
+      });
+    }
+    else if (!instances.isDefault() && !config.isDefault() && !handler.isDefault()) {
+      Container.instance.deployVerticle(moduleName.toJavaString(), new JsonObject(config.toJavaMap(env, new HashMap<String, Object>().getClass())), instances.toJavaInteger());
+    }
+  }
+
+  /**
+   * Undeploys a verticle.
+   */
+  public static void undeployVerticle(final Env env, StringValue deploymentID, @Optional final Callback handler) {
+    if (handler != null && !handler.isDefault()) {
+      Container.instance.undeployVerticle(deploymentID.toString(), new AsyncResultHandler<Void>() {
+        @Override
+        public void handle(AsyncResult<Void> result) {
+          handler.call(env, env.wrapJava(result));
+        }
+      });
+    }
+    else {
+      Container.instance.undeployVerticle(deploymentID.toString());
+    }
+  }
+
+  /**
+   * Deploys a worker verticle.
+   */
+  public static void deployWorkerVerticle(final Env env, final StringValue moduleName, @Optional final ArrayValue config, @Optional final Value instances, @Optional final Callback handler) {
+    if (instances.isDefault() && config.isDefault() && handler.isDefault()) {
+      Container.instance.deployWorkerVerticle(moduleName.toJavaString());
+    }
+    else if (!instances.isDefault() && config.isDefault() && handler.isDefault()) {
+      Container.instance.deployWorkerVerticle(moduleName.toJavaString(), instances.toJavaInteger());
+    }
+    else if (!instances.isDefault() && !config.isDefault() && !handler.isDefault()) {
+      Container.instance.deployWorkerVerticle(moduleName.toJavaString(), new JsonObject(config.toJavaMap(env, new HashMap<String, Object>().getClass())), instances.toJavaInteger());
+    }
+  }
+
+  /**
+   * Returns the current Vertx container environment.
+   */
   public static Map<String, String> env(Env env) {
     return Container.instance.env();
   }
 
+  /**
+   * Exits the container.
+   */
   public static void exit() {
     Container.instance.exit();
   }
 
+  /**
+   * Returns the Vertx logger.
+   */
   public static Logger logger() {
     return Container.instance.logger();
+  }
+
+  /**
+   * Returns the Vertx configuration.
+   */
+  public static JsonObject config(Env env) {
+    return Container.instance.config();
   }
 
 }
