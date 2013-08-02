cfe69b6bb59238fed70f260b2208cf1223456e95
Jordan Halterman <jordan.halterman@gmail.com>
2013-08-02 00:00:02 -0700

    Update RouterMatcher implementation to use new PHP Handlers.

--- src/main/java/com/blankstyle/vertx/php/http/RouteMatcher.java
+++ src/main/java/com/blankstyle/vertx/php/http/RouteMatcher.java
@@ -1,8 +1,7 @@
 package com.blankstyle.vertx.php.http;
 
-import org.vertx.java.core.Handler;
-import org.vertx.java.core.http.HttpServerRequest;
-
+import com.blankstyle.vertx.php.Handler;
+import com.blankstyle.vertx.php.ArgumentModifier;
 import com.caucho.quercus.env.Callback;
 import com.caucho.quercus.env.Env;
 import com.caucho.quercus.env.StringValue;
@@ -22,11 +21,12 @@ public class RouteMatcher {
    * Specify a handler that will be called for all HTTP methods.
    */
   public RouteMatcher all(final Env env, final StringValue pattern, final Callback callback) {
-    matcher.all(pattern.toString(), new Handler<HttpServerRequest>() {
-      public void handle(HttpServerRequest request) {
-        callback.call(env, env.wrapJava(request));
+    matcher.all(pattern.toString(), new Handler<org.vertx.java.core.http.HttpServerRequest>(env, callback, new ArgumentModifier<org.vertx.java.core.http.HttpServerRequest, HttpServerRequest>() {
+      @Override
+      public HttpServerRequest modify(org.vertx.java.core.http.HttpServerRequest request) {
+        return new HttpServerRequest(request);
       }
-    });
+    }));
     return this;
   }
 
@@ -34,11 +34,12 @@ public class RouteMatcher {
    * Specify a handler that will be called for all HTTP methods.
    */
   public RouteMatcher allWithRegex(final Env env, final StringValue regex, final Callback callback) {
-    matcher.allWithRegEx(regex.toString(), new Handler<HttpServerRequest>() {
-      public void handle(HttpServerRequest request) {
-        callback.call(env, env.wrapJava(request));
+    matcher.allWithRegEx(regex.toString(), new Handler<org.vertx.java.core.http.HttpServerRequest>(env, callback, new ArgumentModifier<org.vertx.java.core.http.HttpServerRequest, HttpServerRequest>() {
+      @Override
+      public HttpServerRequest modify(org.vertx.java.core.http.HttpServerRequest request) {
+        return new HttpServerRequest(request);
       }
-    });
+    }));
     return this;
   }
 
@@ -46,11 +47,12 @@ public class RouteMatcher {
    * Specify a handler that will be called for a matching HTTP CONNECT.
    */
   public RouteMatcher connect(final Env env, final StringValue pattern, final Callback callback) {
-    matcher.connect(pattern.toString(), new Handler<HttpServerRequest>() {
-      public void handle(HttpServerRequest request) {
-        callback.call(env, env.wrapJava(request));
+    matcher.connect(pattern.toString(), new Handler<org.vertx.java.core.http.HttpServerRequest>(env, callback, new ArgumentModifier<org.vertx.java.core.http.HttpServerRequest, HttpServerRequest>() {
+      @Override
+      public HttpServerRequest modify(org.vertx.java.core.http.HttpServerRequest request) {
+        return new HttpServerRequest(request);
       }
-    });
+    }));
     return this;
   }
 
@@ -58,11 +60,12 @@ public class RouteMatcher {
    * Specify a handler that will be called for a matching HTTP CONNECT.
    */
   public RouteMatcher connectWithRegex(final Env env, final StringValue regex, final Callback callback) {
-    matcher.connectWithRegEx(regex.toString(), new Handler<HttpServerRequest>() {
-      public void handle(HttpServerRequest request) {
-        callback.call(env, env.wrapJava(request));
+    matcher.connectWithRegEx(regex.toString(), new Handler<org.vertx.java.core.http.HttpServerRequest>(env, callback, new ArgumentModifier<org.vertx.java.core.http.HttpServerRequest, HttpServerRequest>() {
+      @Override
+      public HttpServerRequest modify(org.vertx.java.core.http.HttpServerRequest request) {
+        return new HttpServerRequest(request);
       }
-    });
+    }));
     return this;
   }
 
@@ -70,11 +73,12 @@ public class RouteMatcher {
    * Specify a handler that will be called for a matching HTTP GET.
    */
   public RouteMatcher get(final Env env, final StringValue pattern, final Callback callback) {
-    matcher.get(pattern.toString(), new Handler<HttpServerRequest>() {
-      public void handle(HttpServerRequest request) {
-        callback.call(env, env.wrapJava(request));
+    matcher.get(pattern.toString(), new Handler<org.vertx.java.core.http.HttpServerRequest>(env, callback, new ArgumentModifier<org.vertx.java.core.http.HttpServerRequest, HttpServerRequest>() {
+      @Override
+      public HttpServerRequest modify(org.vertx.java.core.http.HttpServerRequest request) {
+        return new HttpServerRequest(request);
       }
-    });
+    }));
     return this;
   }
 
@@ -82,11 +86,12 @@ public class RouteMatcher {
    * Specify a handler that will be called for a matching HTTP GET.
    */
   public RouteMatcher getWithRegex(final Env env, final StringValue regex, final Callback callback) {
-    matcher.getWithRegEx(regex.toString(), new Handler<HttpServerRequest>() {
-      public void handle(HttpServerRequest request) {
-        callback.call(env, env.wrapJava(request));
+    matcher.getWithRegEx(regex.toString(), new Handler<org.vertx.java.core.http.HttpServerRequest>(env, callback, new ArgumentModifier<org.vertx.java.core.http.HttpServerRequest, HttpServerRequest>() {
+      @Override
+      public HttpServerRequest modify(org.vertx.java.core.http.HttpServerRequest request) {
+        return new HttpServerRequest(request);
       }
-    });
+    }));
     return this;
   }
 
@@ -94,11 +99,12 @@ public class RouteMatcher {
    * Specify a handler that will be called for a matching HTTP POST.
    */
   public RouteMatcher post(final Env env, final StringValue pattern, final Callback callback) {
-    matcher.post(pattern.toString(), new Handler<HttpServerRequest>() {
-      public void handle(HttpServerRequest request) {
-        callback.call(env, env.wrapJava(request));
+    matcher.post(pattern.toString(), new Handler<org.vertx.java.core.http.HttpServerRequest>(env, callback, new ArgumentModifier<org.vertx.java.core.http.HttpServerRequest, HttpServerRequest>() {
+      @Override
+      public HttpServerRequest modify(org.vertx.java.core.http.HttpServerRequest request) {
+        return new HttpServerRequest(request);
       }
-    });
+    }));
     return this;
   }
 
@@ -106,11 +112,12 @@ public class RouteMatcher {
    * Specify a handler that will be called for a matching HTTP POST.
    */
   public RouteMatcher postWithRegex(final Env env, final StringValue regex, final Callback callback) {
-    matcher.postWithRegEx(regex.toString(), new Handler<HttpServerRequest>() {
-      public void handle(HttpServerRequest request) {
-        callback.call(env, env.wrapJava(request));
+    matcher.postWithRegEx(regex.toString(), new Handler<org.vertx.java.core.http.HttpServerRequest>(env, callback, new ArgumentModifier<org.vertx.java.core.http.HttpServerRequest, HttpServerRequest>() {
+      @Override
+      public HttpServerRequest modify(org.vertx.java.core.http.HttpServerRequest request) {
+        return new HttpServerRequest(request);
       }
-    });
+    }));
     return this;
   }
 
@@ -118,11 +125,12 @@ public class RouteMatcher {
    * Specify a handler that will be called for a matching HTTP PUT.
    */
   public RouteMatcher put(final Env env, final StringValue pattern, final Callback callback) {
-    matcher.put(pattern.toString(), new Handler<HttpServerRequest>() {
-      public void handle(HttpServerRequest request) {
-        callback.call(env, env.wrapJava(request));
+    matcher.put(pattern.toString(), new Handler<org.vertx.java.core.http.HttpServerRequest>(env, callback, new ArgumentModifier<org.vertx.java.core.http.HttpServerRequest, HttpServerRequest>() {
+      @Override
+      public HttpServerRequest modify(org.vertx.java.core.http.HttpServerRequest request) {
+        return new HttpServerRequest(request);
       }
-    });
+    }));
     return this;
   }
 
@@ -130,11 +138,12 @@ public class RouteMatcher {
    * Specify a handler that will be called for a matching HTTP PUT.
    */
   public RouteMatcher putWithRegex(final Env env, final StringValue regex, final Callback callback) {
-    matcher.putWithRegEx(regex.toString(), new Handler<HttpServerRequest>() {
-      public void handle(HttpServerRequest request) {
-        callback.call(env, env.wrapJava(request));
+    matcher.putWithRegEx(regex.toString(), new Handler<org.vertx.java.core.http.HttpServerRequest>(env, callback, new ArgumentModifier<org.vertx.java.core.http.HttpServerRequest, HttpServerRequest>() {
+      @Override
+      public HttpServerRequest modify(org.vertx.java.core.http.HttpServerRequest request) {
+        return new HttpServerRequest(request);
       }
-    });
+    }));
     return this;
   }
 
@@ -142,11 +151,12 @@ public class RouteMatcher {
    * Specify a handler that will be called for a matching HTTP DELETE.
    */
   public RouteMatcher delete(final Env env, final StringValue pattern, final Callback callback) {
-    matcher.delete(pattern.toString(), new Handler<HttpServerRequest>() {
-      public void handle(HttpServerRequest request) {
-        callback.call(env, env.wrapJava(request));
+    matcher.delete(pattern.toString(), new Handler<org.vertx.java.core.http.HttpServerRequest>(env, callback, new ArgumentModifier<org.vertx.java.core.http.HttpServerRequest, HttpServerRequest>() {
+      @Override
+      public HttpServerRequest modify(org.vertx.java.core.http.HttpServerRequest request) {
+        return new HttpServerRequest(request);
       }
-    });
+    }));
     return this;
   }
 
@@ -154,11 +164,12 @@ public class RouteMatcher {
    * Specify a handler that will be called for a matching HTTP DELETE.
    */
   public RouteMatcher deleteWithRegex(final Env env, final StringValue regex, final Callback callback) {
-    matcher.deleteWithRegEx(regex.toString(), new Handler<HttpServerRequest>() {
-      public void handle(HttpServerRequest request) {
-        callback.call(env, env.wrapJava(request));
+    matcher.deleteWithRegEx(regex.toString(), new Handler<org.vertx.java.core.http.HttpServerRequest>(env, callback, new ArgumentModifier<org.vertx.java.core.http.HttpServerRequest, HttpServerRequest>() {
+      @Override
+      public HttpServerRequest modify(org.vertx.java.core.http.HttpServerRequest request) {
+        return new HttpServerRequest(request);
       }
-    });
+    }));
     return this;
   }
 
@@ -166,11 +177,12 @@ public class RouteMatcher {
    * Specify a handler that will be called for a matching HTTP HEAD.
    */
   public RouteMatcher head(final Env env, final StringValue pattern, final Callback callback) {
-    matcher.head(pattern.toString(), new Handler<HttpServerRequest>() {
-      public void handle(HttpServerRequest request) {
-        callback.call(env, env.wrapJava(request));
+    matcher.head(pattern.toString(), new Handler<org.vertx.java.core.http.HttpServerRequest>(env, callback, new ArgumentModifier<org.vertx.java.core.http.HttpServerRequest, HttpServerRequest>() {
+      @Override
+      public HttpServerRequest modify(org.vertx.java.core.http.HttpServerRequest request) {
+        return new HttpServerRequest(request);
       }
-    });
+    }));
     return this;
   }
 
@@ -178,11 +190,12 @@ public class RouteMatcher {
    * Specify a handler that will be called for a matching HTTP HEAD.
    */
   public RouteMatcher headWithRegex(final Env env, final StringValue regex, final Callback callback) {
-    matcher.headWithRegEx(regex.toString(), new Handler<HttpServerRequest>() {
-      public void handle(HttpServerRequest request) {
-        callback.call(env, env.wrapJava(request));
+    matcher.headWithRegEx(regex.toString(), new Handler<org.vertx.java.core.http.HttpServerRequest>(env, callback, new ArgumentModifier<org.vertx.java.core.http.HttpServerRequest, HttpServerRequest>() {
+      @Override
+      public HttpServerRequest modify(org.vertx.java.core.http.HttpServerRequest request) {
+        return new HttpServerRequest(request);
       }
-    });
+    }));
     return this;
   }
 
@@ -190,11 +203,12 @@ public class RouteMatcher {
    * Specify a handler that will be called for a matching HTTP TRACE.
    */
   public RouteMatcher trace(final Env env, final StringValue pattern, final Callback callback) {
-    matcher.trace(pattern.toString(), new Handler<HttpServerRequest>() {
-      public void handle(HttpServerRequest request) {
-        callback.call(env, env.wrapJava(request));
+    matcher.trace(pattern.toString(), new Handler<org.vertx.java.core.http.HttpServerRequest>(env, callback, new ArgumentModifier<org.vertx.java.core.http.HttpServerRequest, HttpServerRequest>() {
+      @Override
+      public HttpServerRequest modify(org.vertx.java.core.http.HttpServerRequest request) {
+        return new HttpServerRequest(request);
       }
-    });
+    }));
     return this;
   }
 
@@ -202,11 +216,12 @@ public class RouteMatcher {
    * Specify a handler that will be called for a matching HTTP TRACE.
    */
   public RouteMatcher traceWithRegex(final Env env, final StringValue regex, final Callback callback) {
-    matcher.traceWithRegEx(regex.toString(), new Handler<HttpServerRequest>() {
-      public void handle(HttpServerRequest request) {
-        callback.call(env, env.wrapJava(request));
+    matcher.traceWithRegEx(regex.toString(), new Handler<org.vertx.java.core.http.HttpServerRequest>(env, callback, new ArgumentModifier<org.vertx.java.core.http.HttpServerRequest, HttpServerRequest>() {
+      @Override
+      public HttpServerRequest modify(org.vertx.java.core.http.HttpServerRequest request) {
+        return new HttpServerRequest(request);
       }
-    });
+    }));
     return this;
   }
 
@@ -214,11 +229,12 @@ public class RouteMatcher {
    * Specify a handler that will be called for a matching HTTP OPTIONS.
    */
   public RouteMatcher options(final Env env, final StringValue pattern, final Callback callback) {
-    matcher.options(pattern.toString(), new Handler<HttpServerRequest>() {
-      public void handle(HttpServerRequest request) {
-        callback.call(env, env.wrapJava(request));
+    matcher.options(pattern.toString(), new Handler<org.vertx.java.core.http.HttpServerRequest>(env, callback, new ArgumentModifier<org.vertx.java.core.http.HttpServerRequest, HttpServerRequest>() {
+      @Override
+      public HttpServerRequest modify(org.vertx.java.core.http.HttpServerRequest request) {
+        return new HttpServerRequest(request);
       }
-    });
+    }));
     return this;
   }
 
@@ -226,11 +242,12 @@ public class RouteMatcher {
    * Specify a handler that will be called for a matching HTTP OPTIONS.
    */
   public RouteMatcher optionsWithRegex(final Env env, final StringValue regex, final Callback callback) {
-    matcher.optionsWithRegEx(regex.toString(), new Handler<HttpServerRequest>() {
-      public void handle(HttpServerRequest request) {
-        callback.call(env, env.wrapJava(request));
+    matcher.optionsWithRegEx(regex.toString(), new Handler<org.vertx.java.core.http.HttpServerRequest>(env, callback, new ArgumentModifier<org.vertx.java.core.http.HttpServerRequest, HttpServerRequest>() {
+      @Override
+      public HttpServerRequest modify(org.vertx.java.core.http.HttpServerRequest request) {
+        return new HttpServerRequest(request);
       }
-    });
+    }));
     return this;
   }
 
@@ -238,11 +255,12 @@ public class RouteMatcher {
    * Specify a handler that will be called for a matching HTTP PATCH.
    */
   public RouteMatcher patch(final Env env, final StringValue pattern, final Callback callback) {
-    matcher.patch(pattern.toString(), new Handler<HttpServerRequest>() {
-      public void handle(HttpServerRequest request) {
-        callback.call(env, env.wrapJava(request));
+    matcher.patch(pattern.toString(), new Handler<org.vertx.java.core.http.HttpServerRequest>(env, callback, new ArgumentModifier<org.vertx.java.core.http.HttpServerRequest, HttpServerRequest>() {
+      @Override
+      public HttpServerRequest modify(org.vertx.java.core.http.HttpServerRequest request) {
+        return new HttpServerRequest(request);
       }
-    });
+    }));
     return this;
   }
 
@@ -250,11 +268,12 @@ public class RouteMatcher {
    * Specify a handler that will be called for a matching HTTP PATCH.
    */
   public RouteMatcher patchWithRegex(final Env env, final StringValue regex, final Callback callback) {
-    matcher.patchWithRegEx(regex.toString(), new Handler<HttpServerRequest>() {
-      public void handle(HttpServerRequest request) {
-        callback.call(env, env.wrapJava(request));
+    matcher.patchWithRegEx(regex.toString(), new Handler<org.vertx.java.core.http.HttpServerRequest>(env, callback, new ArgumentModifier<org.vertx.java.core.http.HttpServerRequest, HttpServerRequest>() {
+      @Override
+      public HttpServerRequest modify(org.vertx.java.core.http.HttpServerRequest request) {
+        return new HttpServerRequest(request);
       }
-    });
+    }));
     return this;
   }
 
@@ -262,18 +281,19 @@ public class RouteMatcher {
    * Specify a handler that will be called when no other handlers match.
    */
   public RouteMatcher noMatch(final Env env, final Callback callback) {
-    matcher.noMatch(new Handler<HttpServerRequest>() {
-      public void handle(HttpServerRequest request) {
-        callback.call(env, env.wrapJava(request));
+    matcher.noMatch(new Handler<org.vertx.java.core.http.HttpServerRequest>(env, callback, new ArgumentModifier<org.vertx.java.core.http.HttpServerRequest, HttpServerRequest>() {
+      @Override
+      public HttpServerRequest modify(org.vertx.java.core.http.HttpServerRequest request) {
+        return new HttpServerRequest(request);
       }
-    });
+    }));
     return this;
   }
 
   /**
    * Something has happened, so handle it.
    */
-  public void handle(Env env, HttpServerRequest request) {
+  public void handle(Env env, org.vertx.java.core.http.HttpServerRequest request) {
     matcher.handle(request);
   }
 
